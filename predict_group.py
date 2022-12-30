4171898c1f5e3e7406ac51c6f5e9a5c335018c73
r11922123 <breakxing1102@gmail.com>
2022-12-30 16:17:50 +0800

    Add parser to predict_group.py

--- predict_group.py
+++ predict_group.py
@@ -1,13 +1,9 @@
 import argparse
 import csv
-import numpy as np
-import pandas as pd
 from transformers import AutoModelForSequenceClassification
 from datasets import load_dataset
 from transformers import AutoTokenizer
-from transformers import TrainingArguments, Trainer
-from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
-from transformers import EvalPrediction
+from transformers import Trainer
 import torch
 from tqdm import tqdm
 parser = argparse.ArgumentParser()
@@ -17,102 +13,60 @@ parser.add_argument(
         help="Path to fine tuned model directory.",
         required=True,
     )
+parser.add_argument(
+        "-t",
+        type=str,
+        help="Path to processed test file.",
+        required=True,
+    )
+parser.add_argument(
+        "-o",
+        type=str,
+        help="Path to output csv file.",
+        required=True,
+    )
+parser.add_argument(
+        "-max_length",
+        type=int,
+        default=512,
+        help="Tokenizer's max_length.",
+    )
 args = parser.parse_args()
-
 data_files = {}
-data_files["train"] = 'processed_train_group_2.csv'
-data_files["validation"] = 'processed_val_group_2.csv'
+data_files["test"] = args.t
 extension="csv"
 dataset = load_dataset(extension, data_files=data_files)
-labels = [label for label in dataset['train'].features.keys() if label not in ['user_id', 'interests']]
+labels = [label for label in dataset['test'].features.keys() if label not in ['user_id', 'interests']]
 id2label = {idx:label for idx, label in enumerate(labels)}
 label2id = {label:idx for idx, label in enumerate(labels)}
-tokenizer = AutoTokenizer.from_pretrained(args.m)
-def preprocess_data(examples):
-    # take a batch of texts
-    text = examples["interests"]
-    # encode them
-    encoding = tokenizer(text, padding="max_length", truncation=True, max_length=512)
-    # add labels
-    labels_batch = {k: examples[k] for k in examples.keys() if k in labels}
-    # create numpy array of shape (batch_size, num_labels)
-    labels_matrix = np.zeros((len(text), len(labels)))
-    # fill numpy array
-    for idx, label in enumerate(labels):
-        labels_matrix[:, idx] = labels_batch[label]
-    encoding["labels"] = labels_matrix.tolist()
-    
-    return encoding
 
-encoded_dataset = dataset.map(preprocess_data, batched=True, remove_columns=dataset['train'].column_names)
-encoded_dataset.set_format("torch")
+tokenizer = AutoTokenizer.from_pretrained(args.m)
+
 model = AutoModelForSequenceClassification.from_pretrained(args.m, 
                                                            problem_type="multi_label_classification", 
                                                            num_labels=len(labels),
                                                            id2label=id2label,
                                                            label2id=label2id)
-batch_size = 4
-metric_name = "f1"
-args = TrainingArguments(
-    args.m,
-    evaluation_strategy = "epoch",
-    save_strategy = "epoch",
-    learning_rate=2e-5,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    num_train_epochs=5,
-    weight_decay=0.01,
-    load_best_model_at_end=True,
-    gradient_accumulation_steps=4,
-    metric_for_best_model=metric_name,
-)
-def multi_label_metrics(predictions, labels, threshold=0.5):
-    # first, apply sigmoid on predictions which are of shape (batch_size, num_labels)
-    sigmoid = torch.nn.Sigmoid()
-    probs = sigmoid(torch.Tensor(predictions))
-    # next, use threshold to turn them into integer predictions
-    y_pred = np.zeros(probs.shape)
-    y_pred[np.where(probs >= threshold)] = 1
-    # finally, compute metrics
-    y_true = labels
-    f1_micro_average = f1_score(y_true=y_true, y_pred=y_pred, average='micro')
-    roc_auc = roc_auc_score(y_true, y_pred, average = 'micro')
-    accuracy = accuracy_score(y_true, y_pred)
-    # return as dictionary
-    metrics = {'f1': f1_micro_average,
-               'roc_auc': roc_auc,
-               'accuracy': accuracy}
-    return metrics
 
-def compute_metrics(p: EvalPrediction):
-    preds = p.predictions[0] if isinstance(p.predictions, 
-            tuple) else p.predictions
-    result = multi_label_metrics(
-        predictions=preds, 
-        labels=p.label_ids)
-    return result
+
 
 trainer = Trainer(
     model,
-    args,
-    train_dataset=encoded_dataset["train"],
-    eval_dataset=encoded_dataset["validation"],
     tokenizer=tokenizer,
-    compute_metrics=compute_metrics
 )
 text_lst=[]
-with open('processed_test_group_2.csv','r',encoding='utf-8')as f:
+with open(args.t,'r',encoding='utf-8')as f:
     ds=csv.DictReader(f)
     for d in ds:
         text_lst.append(d)
 
-with open('output_test.csv','w',encoding='utf-8',newline='') as f:
+with open(args.o,'w',encoding='utf-8',newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['user_id', 'subgroup'])
     for data in tqdm(text_lst):
         id=data['user_id']
         text=data['interests']
-        encoding = tokenizer(text, return_tensors="pt",padding="max_length", truncation=True,max_length=512)
+        encoding = tokenizer(text, return_tensors="pt",padding="max_length", truncation=True,max_length=args.max_length)
         encoding = {k: v.to(trainer.model.device) for k,v in encoding.items()}
         outputs = trainer.model(**encoding)
         logits = outputs.logits
